d858f91d02320052e84b44583592128940f683d7
Mustafa Battaloglu <mmbattaloglu@gmail.com>
2023-08-31 13:56:17 +0300

    Singleton

--- Assets/Sctipts/Singleton/PlayerInput.cs
+++ Assets/Sctipts/Singleton/PlayerInput.cs
@@ -1,21 +1,15 @@
 using UnityEngine;
+
 namespace DesignPatterns.Singleton
 {
     public class PlayerInput : MonoBehaviour
     {
-        private PlayerScore playerScore;
-
-        private void Start()
-        {
-            playerScore = FindObjectOfType<PlayerScore>();
-        }
-
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 Debug.Log("Space key pressed");
-                playerScore.AddScore(1);
+                PlayerScore.Instance.AddScore(1);
             }
         }
     }
--- Assets/Sctipts/Singleton/PlayerScore.cs
+++ Assets/Sctipts/Singleton/PlayerScore.cs
@@ -1,12 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace DesignPatterns.Singleton
 {
-
     public class PlayerScore : MonoBehaviour
     {
+        public static PlayerScore Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
         private int score = 0;
 
         public void AddScore(int amount)
